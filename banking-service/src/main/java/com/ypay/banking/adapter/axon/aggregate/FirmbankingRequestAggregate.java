f7db5c50a00ba2611d3720dd55d2382220cf1007
yssy3135 <ys3135@gmail.com>
2024-04-04 23:52:37 +0900

    feat(saga): request-firmbanking
    - request-firmbanking saga 구성

--- banking-service/src/main/java/com/ypay/banking/adapter/axon/aggregate/FirmbankingRequestAggregate.java
+++ banking-service/src/main/java/com/ypay/banking/adapter/axon/aggregate/FirmbankingRequestAggregate.java
@@ -2,6 +2,13 @@ package com.ypay.banking.adapter.axon.aggregate;
 
 import com.ypay.banking.adapter.axon.command.CreateFirmbankingRequestCommand;
 import com.ypay.banking.adapter.axon.event.FirmbankingRequestCreatedEvent;
+import com.ypay.banking.adapter.out.external.bank.ExternalFirmbankingRequest;
+import com.ypay.banking.adapter.out.external.bank.FirmbankingResult;
+import com.ypay.banking.application.port.out.RequestExternalFirmbankingPort;
+import com.ypay.banking.application.port.out.RequestFirmbankingPort;
+import com.ypay.banking.domain.FirmbankingRequest;
+import com.ypay.common.event.RequestFirmbankingCommand;
+import com.ypay.common.event.RequestFirmbankingFinishedEvent;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.axonframework.commandhandling.CommandHandler;
@@ -39,6 +46,46 @@ public class FirmbankingRequestAggregate {
         apply(new FirmbankingRequestCreatedEvent(command.getFromBankName(), command.getFromBankAccountNumber(), command.getToBankName(), command.getToBankAccountNumber(), command.getMoneyAmount()));
     }
 
+    @CommandHandler
+    public FirmbankingRequestAggregate(RequestFirmbankingCommand command, RequestFirmbankingPort firmbankingPort, RequestExternalFirmbankingPort externalFirmbankingPort){
+        System.out.println("FirmbankingRequestAggregate Handler");
+        id = command.getAggregateIdentifier();
+
+        // from -> to
+        // 펌뱅킹 수행!
+        firmbankingPort.createFirmbankingRequest(
+                new FirmbankingRequest.FromBankName(command.getToBankName()),
+                new FirmbankingRequest.FromBankAccountNumber(command.getToBankAccountNumber()),
+                new FirmbankingRequest.ToBankName("ybank"),
+                new FirmbankingRequest.ToBankAccountNumber("123-333-9999"),
+                new FirmbankingRequest.MoneyAmount(command.getMoneyAmount()),
+                new FirmbankingRequest.FirmbankingStatus(0),
+                new FirmbankingRequest.FirmbankingAggregateIdentifier(id));
+
+        // firmbanking!
+        FirmbankingResult firmbankingResult = externalFirmbankingPort.requestExternalFirmbanking(
+                new ExternalFirmbankingRequest(
+                        command.getFromBankName(),
+                        command.getFromBankAccountNumber(),
+                        command.getToBankName(),
+                        command.getToBankAccountNumber(),
+                        command.getMoneyAmount()
+                ));
+
+        int resultCode = firmbankingResult.getResultCode();
+
+        // 0. 성공, 1. 실패
+        apply(new RequestFirmbankingFinishedEvent(
+                command.getRequestFirmbankingId(),
+                command.getRechargeRequestId(),
+                command.getMembershipId(),
+                command.getToBankName(),
+                command.getToBankAccountNumber(),
+                command.getMoneyAmount(),
+                resultCode,
+                id
+        ));
+    }
 
     @EventSourcingHandler
     public void on (FirmbankingRequestCreatedEvent event) {
