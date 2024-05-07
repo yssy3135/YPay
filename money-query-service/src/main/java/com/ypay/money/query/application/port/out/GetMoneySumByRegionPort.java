798a44b1a8c3ef4c9e946e4bec3b3edfc93f24f3
yssy3135 <ys3135@gmail.com>
2024-05-07 23:52:24 +0900

    feat(api): 지역별 money 합계를 구하기 위한 dynamo 구성
    - 지역별 money 합계를 구하기 위한 api 통신
    - dynamoDB 구성

--- money-query-service/src/main/java/com/ypay/money/query/application/port/out/GetMoneySumByRegionPort.java
+++ money-query-service/src/main/java/com/ypay/money/query/application/port/out/GetMoneySumByRegionPort.java
@@ -1,4 +1,7 @@
 package com.ypay.money.query.application.port.out;
 
+import java.util.Date;
+
 public interface GetMoneySumByRegionPort {
+    MoneySum getMoneySumByRegionPort(String regionName, Date startDate);
 }
